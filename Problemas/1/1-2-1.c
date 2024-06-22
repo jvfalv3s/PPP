3d91c342d9ae0b2c6169040dba7f193c6fe3dca6
Joao Alves <jvfalves@gmail.com>
2024-06-22 23:43:45 +0100

    adição dos exercicios em ordem ate 3-1-2

--- Problemas/1/1-2-1.c
+++ Problemas/1/1-2-1.c
@@ -8,7 +8,34 @@ int main(){
     int notas[] = {100, 50, 20, 10, 5};
     float moedas[] = {2, 1, 0.5, 0.2, 0.1, 0.05, 0.01};
 
-    
+    float valor;
 
+    printf("Introduza o valor da compra: ");
+    scanf("%f", &valor);
+
+    if(valor >= 100){
+        printf("Valor invalido\n");
+        return 1;
+    }
+
+    printf("Notas:\n");
+    for(int i = 0; i < 5; i++){
+        int n = valor / notas[i];
+        if(n > 0){
+            printf("%d nota(s) de %d euros\n", n, notas[i]);
+            valor -= n * notas[i];
+        }
+    }
+
+    printf("Moedas:\n");
+
+    //o menor numeros de moedas
+    for(int i = 0; i < 7; i++) { // Adjusted for the length of the updated moedas array
+        int n = valor / moedas[i];
+        if(n > 0) {
+            printf("%d moeda(s) de %.2f euros\n", n, moedas[i] / 100.0); // Convert cents back to euros for display
+            valor -= n * moedas[i];
+        }
+    }
     return 0;
 }
--- Problemas/2/2-2-4.c
+++ Problemas/2/2-2-4.c
@@ -1,27 +1,29 @@
 //escreva um programa que determine  a que estacao do ano pertence uma data ( dia e mes ) fornecida pelo utilizador.
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-    char estacao[4] = {"Inverno", "Primavera", "Verao", "Outono"};
     int dia, mes;
+    printf("Introduza o dia e o mes: ");
+    scanf("%d %d", &dia, &mes);
 
-    printf("Por favor digite o dia: ");
-    scanf("%d", &dia);
-    printf("Por favor digite o mes: ");
-    scanf("%d", &mes);
+    if(mes == 1 || mes == 2 || mes == 3){
+        printf("Primavera\n");
+    }
 
-    if(mes < 3 || (mes == 3 && dia < 21) || (mes == 6 && dia > 20)){
-        printf("Estamos na estacao do ano: %s", estacao[0]);
-    } else if (mes < 6 || (mes == 6 && dia < 21) || (mes == 9 && dia > 20)){
-        printf("Estamos na estacao do ano: %s", estacao[1]);
-    } else if (mes < 9 || (mes == 9 && dia < 21) || (mes == 12 && dia > 20)){
-        printf("Estamos na estacao do ano: %s", estacao[2]);
-    } else {
-        printf("Estamos na estacao do ano: %s", estacao[3]);
+    if(mes == 4 || mes == 5 || mes == 6){
+        printf("Verao\n");
+    }
+
+    if(mes == 7 || mes == 8 || mes == 9){
+        printf("Outono\n");
+    }
+
+    if(mes == 10 || mes == 11 || mes == 12){
+        printf("Inverno\n");
     }
 
     return 0;
 }
-
-//errado e pedir ao proff para corrigir.
+//corrigido e correto.
