8ecfd4e602b639d17d1a867805ad3c004c31d41e
Joao Vitor Alves <jvfalves@gmail.com>
2026-04-22 03:15:08 +0100

    "problemas de nivel facil de ponteiros"

--- Problemas/Problemas3/8.Apontadores/8.1.NivelBasico/exe2.c
+++ Problemas/Problemas3/8.Apontadores/8.1.NivelBasico/exe2.c
@@ -3,11 +3,31 @@
 //percorrer um vetor de 12 inteiros (pre-definidos) e determine a media desses valores 
 //usando apontadores para percorrer o vetor
 
-int media(){
+float media(int *ptr, int tamanho){
 
-    int vetor[12] = {2,3,6,4,8,10,2,3,4,4,9,1}
+    int soma = 0;
+    int *fim = ptr + tamanho; //Ponteiro que marca o limite do vetor
+
+    //while pra percorrer o vetor usando ponteiros
+    while (ptr < fim){
+        soma += *ptr;
+        ptr++;
+    }
+
+    return (float)soma/tamanho; // retorna a media fazendo casting de soma para float
+
+}
 
 
-    
 
+int main (){
+
+    int vetor[12] = {2,3,6,4,8,10,2,3,4,4,9,1};
+    int n = 12;
+
+    float media1 = media(vetor, n);
+
+    printf(" A media dos valores e: %.2f\n", media1);
+
+    return 0;
 }
