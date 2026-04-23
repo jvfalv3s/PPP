caf23f0dd3a5d4eab379270c82aacc0181953b3b
Joao Vitor Alves <jvfalves@gmail.com>
2026-04-23 16:52:24 +0100

    "tentativa do 13.2.4, mix de stacks com filas"

---
+++ Problemas/Problemas4/13.Listas/13.2.NivelMedio/exe4/lista.h
@@ -0,0 +1,36 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+struct element {
+    int value;
+};
+
+struct list_node {
+    struct element data;
+    struct list_node *next;
+};
+
+//struct da pilha
+struct stack_node {
+    int value;
+    struct stack_node *prox;
+};
+
+//fonções lista geral
+struct list_node *create(void);
+int empty(struct list_node *list);
+void insert(struct list_node *list, struct element e);
+void print_list(const struct list_node *list);
+struct list_node *clean(struct list_node *list);
+
+//funções pilha geral
+void create(struct stack_node **p);
+int empty(const struct stack_node *p);
+int push(struct stack_node **p, int x);
+int pop(struct stack_node **p, int *x);
+void print_stack(const struct stack_node *p);
+void clean(struct stack_node **p);
+
+
+
+#endif
