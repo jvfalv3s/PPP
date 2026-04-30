b2f4db35a04375a885a1230669893ca69e707f62
jvfalv3s <jvfalves@gmail.com>
2026-04-30 16:49:49 +0100

    "problemas que eu tive dificuldade para resolver"

--- Problemas/Problemas2/5.Vetores/5.1.NivelBasico/exe1.c
+++ Problemas/Problemas2/5.Vetores/5.1.NivelBasico/exe1.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 
-int vetorInt[] = [1,2,3,4,5,7];
+int vetorInt[] = {1,2,3,4,5,7};
 
-int dobroImpar(){
+void dobroImpar(){
 
     for(int i = 0; i < 7; i++){
-        
+        if(vetorInt[i] % 2 != 0){
+            vetorInt[i] = vetorInt[i] * 2;
+        }
     }
+    for(int i = 0; i < 7; i++){
+        printf("%d ", vetorInt[i]);
+    }
+}
 
-
+int main(){
+    dobroImpar();
+    return 0;
 }
