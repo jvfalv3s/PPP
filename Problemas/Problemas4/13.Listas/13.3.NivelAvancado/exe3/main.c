01793c643ebfef386148a70b994038e97060a053
jvfalv3s <jvfalves@gmail.com>
2026-04-30 18:54:50 +0100

    "make files e exercicio 13.3.3"

---
+++ Problemas/Problemas4/13.Listas/13.3.NivelAvancado/exe3/main.c
@@ -0,0 +1,8 @@
+//lista quadrplamente ligada, um ponteiro por elemento.
+
+//as delcaracoes podem ser colocadas direro no main
+
+// a mudanca fica nos nos, 4 pointers pra proximo e 4 pra ini
+
+// diferentes tipos de procura caso use difenerntes tipos de dados (int chars)
+
